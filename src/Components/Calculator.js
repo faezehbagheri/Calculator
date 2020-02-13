90ed4561bbcac705d1fe660b7c46df63a288fe9a
faezehbagheri <faezehbagheri98@gmail.com>
2020-02-14 01:08:44 +0330

    add proje

---
+++ src/Components/Calculator.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import './style.css';
+import Result from './Result';
+import CalcButton from "./CalcButton";
+
+class Calculator extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            result: ""
+        }
+    }
+
+    onClick = button => {
+
+        if (button === "=") {
+            this.calculate()
+        }
+
+        else if (button === "C") {
+            this.reset()
+        }
+        else if (button === "CE") {
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        try{
+            this.setState({
+                result: (eval(this.state.result)) + ""
+            })
+        }catch(error){
+            this.setState({
+                result: "error"
+            })
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="calculator-body">
+                    <Result result={this.state.result} />
+                    <CalcButton onClick={this.onClick} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Calculator
